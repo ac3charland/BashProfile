0033360d12e38d3a032314808d1b3f81db46dc82
Alex Charland <ac3charland@gmail.com>
2019-11-21 21:44:29 -0600

    Add git commit shortcut. Fix vlog function

--- profile.sh
+++ profile.sh
@@ -1,13 +1,14 @@
 #Git Shortcuts
 alias status='git status'
 alias push='git push'
+gcam () { git commit -am "$1"; }
 
 #General Shortcuts
 alias editBashProfile='nano ~/code/Misc/BashProfile/profile.sh'
 alias ebp='editBashProfile'
 alias cdBashProfile='cd ~/code/Misc/BashProfile'
 alias cdbp='cdBashProfile'
-vlogme () { sh ~/code/projects/NeistatScript/run.sh "$1" }
+vlogme () { sh ~/code/projects/NeistatScript/run.sh "$1"; }
 
 #Navigation Shortcuts
 alias desktop='cd ~/Desktop'
