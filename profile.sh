96db7413c501c967d6661abcc862babc3ec31d32
Alex Charland <ac3charland@gmail.com>
2019-11-21 21:34:10 -0600

    Initiate document to source in .bash_profile with a few basic commands

---
+++ profile.sh
@@ -0,0 +1,8 @@
+#Git Shortcuts
+alias status='git status'
+alias push='git push'
+
+#General Shortcuts
+alias editBashProfile='nano ~/code/Misc/BashProfile/profile.sh'
+alias ebp='editBashProfile'
+alias vlogme='sh ~/code/projects/NeistatScript/run.sh'
