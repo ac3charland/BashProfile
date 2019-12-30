643031eff4b81792e9f09e0cdec67601666a1afe
Alex Charland <ac3charland@gmail.com>
2019-12-30 09:36:34 -0600

    Add backstop shortcuts

--- profile.sh
+++ profile.sh
@@ -13,6 +13,9 @@ alias editBashProfile='nano ~/code/Misc/BashProfile/profile.sh'
 alias ebp='editBashProfile'
 alias cdBashProfile='cd ~/code/Misc/BashProfile'
 alias cdbp='cdBashProfile'
+alias bs-t='npm run backstop:test'
+alias bs-r='npm run backstop:report'
+alias bs-a='npm run backstop:approve'
 vlogme () { sh ~/code/projects/NeistatScript/run.sh "$1"; }
 
 #Navigation Shortcuts
