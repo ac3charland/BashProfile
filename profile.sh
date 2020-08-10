e3145d56bdaedbc2e101c568ad5e658c2ecb0b44
Alex Charland <ac3charland@gmail.com>
2020-08-10 08:18:58 -0500

    Add shortcut for mac setup script project.

--- profile.sh
+++ profile.sh
@@ -23,6 +23,7 @@ alias portfolio='cd ~/code/projects/react-portfolio-redux/'
 alias tflr='cd ~/code/projects/tflr/'
 alias template='cd ~/code/projects/react-template/'
 alias jacob='cd ~/code/projects/jacob-portfolio/'
+alias mac-setup='cd ~/code/projects/mac-setup-script/'
 
 #Git Shortcuts
 alias status='git status'
